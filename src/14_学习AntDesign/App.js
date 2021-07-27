7ef5b67b0155772b60b1245fda79be46a781320c
jinxb <1659340302@qq.com>
2021-07-27 20:57:30 +0800

    7-27

---
+++ src/14_学习AntDesign/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        {
+          <h2>我是标题 </h2>
+        }
+      </div>
+    )
+  }
+}
