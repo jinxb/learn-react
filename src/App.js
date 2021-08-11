9d48862ff36fe49266f2b0b9b3e429b9884a76ff
jinxb <1659340302@qq.com>
2021-08-11 21:02:13 +0800

    8-11

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+
+import CSSTransition from './transition/CSSTransitionDemo'
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <CSSTransition/>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './react基础/15_axios的使用/App';
+import App from './App';
 
 // import './04_组件通信的案例/style.css'
 // import './05_React实现slot/style.css'
 // import './css/index.css'
+import './transition/CSSTransition.css'
 
 //评论demo
-// import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
 //字体国际化
 // import 'moment/locale/zh-cn'
 
