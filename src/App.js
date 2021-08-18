ec188cc7ab655a8b4e41b2de7b8565d6182a9ccb
jinxb <1659340302@qq.com>
2021-08-18 22:59:38 +0800

    8-18

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends PureComponent {
     return (
       <div>
         <CSSTransition/>
-	该结束的感情，就让它结束吧，茶饭不思、整夜失眠，折磨的不过是自己，潇洒地道声再见就转身走吧。谢谢曾在你生命里浓墨重彩的那个人，谢谢曾经毫无保留的坦诚日子，祝福彼此未来都可以过的好，也会遇见想要的爱情。最好的告别，大抵就是这样，愿对方安好，愿你优雅放下
+	
       </div>
     )
   }
